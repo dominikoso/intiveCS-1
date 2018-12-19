5f7c8e2e088ea29875ed195df21d201895ceeef7
Dominik K <rodzinka.kostkow@gmail.com>
2018-12-19 21:19:26 +0100

    initial commit
    FizzBuzz complete

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void FizzBuzz(int number){
+                if (number % 2 == 0){
+                    Console.Write("Fizz");
+                }if(number % 3 == 0){
+                    Console.Write("Buzz");
+                }  
+        }
+        public static void Main(string[] args)
+        {   
+            FizzBuzz(3);
+        }
+    }
+}
